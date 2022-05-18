c86b72a6511c89922fde829ad7989b1a5bfb869b
Leguan122 <kiss.adam.bs@gmail.com>
2022-05-18 07:06:40 +0200

    create base spring

--- src/main/java/com/example/springLearning/SpringLearningApplication.java
+++ src/main/java/com/example/springLearning/SpringLearningApplication.java
@@ -6,16 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @SpringBootApplication
 @RestController
 public class SpringLearningApplication {
 
-	public static void main(String[] args) {
+	private static final String template = "Hello %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	public static void main(String[] args)
+	{
+
+
 		SpringApplication.run(SpringLearningApplication.class, args);
+
+
 	}
 
-	@GetMapping("/home")
-	public String home(@RequestParam(value = "name", defaultValue = "World") String name){
-		return String.format("Hello %s!", name);
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+		return new Greeting(counter.incrementAndGet(),String.format(template,name));
 	}
 }
